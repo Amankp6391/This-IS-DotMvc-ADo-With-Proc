9f5f2ac974f33b47fda4ff35a78e8c458d0767ff
Amankp6391 <136051717+Amankp6391@users.noreply.github.com>
2024-05-24 15:51:06 +0530

    Add files via upload

---
+++ UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCrudWithAdo.Models
+{
+    public class UserModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "name Is required")]
+        [Display(Name = "user Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "email Is required")]
+        [Display(Name = "email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "age Is required")]
+        [Display(Name = "age")]
+        [Range(18, 60, ErrorMessage = "age must be in between 18 to 60")]
+        public string Age { get; set; }
+    }
+}
